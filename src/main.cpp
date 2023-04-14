e9af17d1b7ebbb6608cf14638765abb75b205ad5
klaudius71 <diliunas.klaudius@gmail.com>
2023-04-13 23:30:41 -0500

    added GLFW ; opened window with no graphics context

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include "GLFW/glfw3.h"
+#include <cassert>
+
+#define UNREFERENCED_PARAMETER(x) x
+
+void glfw_error_callback(int error_code, const char* description)
+{
+    UNREFERENCED_PARAMETER(error_code);
+    printf(description);
+    printf("\n");
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    glfwSetErrorCallback(glfw_error_callback);
+
+    int err;
+    err = glfwInit();
+    assert(err != -1 && "Failed to initialize GLFW!");
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    GLFWwindow* window;
+    window = glfwCreateWindow(1920, 1080, "Iceberg Application", nullptr, nullptr);
+    assert(window && "Failed to create a window!");
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE))
+        {
+            glfwSetWindowShouldClose(window, 1);
+        }
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
 }
