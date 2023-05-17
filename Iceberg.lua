4eaffe8552b8d30cb3f81337e9f47802daf38cd7
klaudius71 <diliunas.klaudius@gmail.com>
2023-05-17 17:09:22 -0500

    reorganized premake to make it possible to add Iceberg as a project without the workspace

---
+++ Iceberg.lua
@@ -0,0 +1,86 @@
+newoption {
+    trigger = "VulkanSDK",
+    value = "API",
+    description = "Choose how to load Vulkan",
+    allowed = {
+       { "sdk",    "SDK" },
+       { "volk",  "volk" }
+    },
+    default = "sdk"
+}
+newoption { 
+    trigger = "include-sandbox",
+    value = "sand",
+    description = "Include the Sandbox project",
+    allowed = {
+        { "no", "No" },
+        { "yes", "Yes" }
+    },
+    default = "no"
+}
+
+group "Dependencies"
+    include "external"
+group ""
+
+project "Iceberg"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    pchheader "ipch.h"
+    pchsource "src/Core/ipch.cpp"
+
+    files
+    {
+        "src/**.h",
+        "src/**.cpp"
+    }
+
+    links 
+    { 
+        "GLFW",
+        "imgui",
+    }
+
+    includedirs 
+    { 
+        "src",
+        "src/Core",
+        "src/GraphicsContext",
+        "src/GraphicsContext/Buffers",
+        "src/GraphicsContext/Pipeline",
+        "src/GraphicsContext/Texture",
+        "external/glfw/include",
+        "external/glm",
+        "external/imgui",
+        "external/stb"
+    }
+    
+    filter { "options:VulkanSDK=sdk" }
+        includedirs { "%{VULKAN_SDK}/Include" }
+        libdirs { "%{VULKAN_SDK}/Lib" }
+        links { "vulkan-1.lib" }
+
+    filter { "options:VulkanSDK=volk" }
+        includedirs 
+        { 
+            "external/volk",
+            "external/Vulkan-Headers/include"
+        }
+        links { "volk" }
+        defines { "ICEBERG_VOLK" }
+
+    filter { "platforms:x64" }
+        architecture "x64"
+
+    filter { "configurations:Debug" }
+        defines { "DEBUG" }
+        symbols "On"
+    
+    filter { "configurations:Release" }
+        defines {"NDEBUG"}
+        optimize "On"
